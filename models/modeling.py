a7395f4bac090e75888c70ec6ca16db7c65572b6
wonchul <onedang22@gmail.com>
2023-03-02 23:24:24 +0900

    edit modeling.py for deeplabv3_

--- models/modeling.py
+++ models/modeling.py
@@ -37,15 +37,15 @@ def get_model(model_name, num_classes, weights=None, weights_backbone=None, aux_
                 model.aux_classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
             else:
                 raise ValueError(f"There is no such backbone({model_name.split('_')[1]}) for {model_name}")
-        elif model.split('_')[0] == 'deeplabv3':
-            model = torchvision.models.segmentation.__dict__[model](pretrained=True, aux_loss=aux_loss)
-            if model.split('_')[1] == 'resnet101': 
+        elif model_name.split('_')[0] == 'deeplabv3':
+            model = torchvision.models.segmentation.__dict__[model_name](pretrained=True, aux_loss=aux_loss)
+            if model_name.split('_')[1] == 'resnet101': 
                 model.classifier = torchvision.models.segmentation.deeplabv3.DeepLabHead(model.backbone.layer4[2].conv3.out_channels, num_classes)
                 model.aux_classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
-            elif model.split('_')[1] == 'resnet50': 
+            elif model_name.split('_')[1] == 'resnet50': 
                 model.classifier = torchvision.models.segmentation.deeplabv3.DeepLabHead(model.backbone.layer4[2].conv3.out_channels, num_classes)
                 model.aux_classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1), stride=(1, 1))
-            elif model.split('_')[1] == 'mobilenet': 
+            elif model_name.split('_')[1] == 'mobilenet': 
                 model.classifier = torchvision.models.segmentation.deeplabv3.DeepLabHead(960, num_classes)
                 model.aux_classifier[4] = torch.nn.Conv2d(10, num_classes, kernel_size=(1, 1), stride=(1, 1))
             else:
