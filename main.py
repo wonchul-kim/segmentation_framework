c7aa360b93d9a0a76752b8ba1b7189d8a463a9a9
wonchul <onedang22@gmail.com>
2023-04-19 15:53:37 +0900

    cv2 torch is working with torch.int64 mask/target dtype

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ class TrainSegmentation(AlgBase):
         self._var_current_epoch = 0
         self._var_current_total_step = 0
         
-        self._var_ml_framework = "tensorflow"
-        # self._var_ml_framework = "pytorch"
+        # self._var_ml_framework = "tensorflow"
+        self._var_ml_framework = "pytorch"
         
     def alg_set_cfgs(self, config="./data/configs/train.yml", info=None, recipe=None, augs=None, option=None):
         super().alg_set_cfgs(config=config, info=info, recipe=recipe, augs=augs, option=option)
@@ -140,8 +140,8 @@ if __name__ == "__main__":
     # info = './data/recipes/projects/train/single_rois_w_patches.yml'
     info = './data/recipes/projects/train/multiple_rois_w_patches.yml'
     # info = './data/projects/sungwoo_u_top_bottom.yml'
-    # recipe = './data/params/train.yml'
-    recipe = './data/params/train_tf.yml'
+    recipe = './data/params/train.yml'
+    # recipe = './data/params/train_tf.yml'
     # augs = "./data/params/augs.yml"
     augs = None
     option = None
